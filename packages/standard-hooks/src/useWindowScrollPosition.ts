4c718be07c87be596a9e4f3f652ee2b9b1db3fb1
Kristóf Poduszló <kripod@protonmail.com>
2019-06-16 09:34:16 +0200

    Optimize RAF-smoothened handlers of hooks

--- packages/standard-hooks/src/useWindowScrollPosition.ts
+++ packages/standard-hooks/src/useWindowScrollPosition.ts
@@ -1,18 +1,21 @@
-import { useState } from 'react';
-import useEventListener from './useEventListener';
-import useSmoothAnimation from './useSmoothAnimation';
-import { canUseDOM } from './utils';
+import { useEffect, useState } from 'react';
+import { canUseDOM, managedEventListener, smoothAnimation } from './utils';
 
 export default function useWindowScrollPosition() {
   const [position, setPosition] = useState(
     canUseDOM ? [window.scrollX, window.scrollY] : [0, 0],
   );
 
-  useEventListener(
-    'scroll',
-    useSmoothAnimation(() => {
-      setPosition([window.scrollX, window.scrollY]);
-    }),
+  useEffect(
+    () =>
+      managedEventListener(
+        window,
+        'scroll',
+        smoothAnimation(() => {
+          setPosition([window.scrollX, window.scrollY]);
+        }),
+      ),
+    [],
   );
 
   return position;
--- packages/standard-hooks/src/useWindowSize.ts
+++ packages/standard-hooks/src/useWindowSize.ts
@@ -1,18 +1,21 @@
-import { useState } from 'react';
-import useEventListener from './useEventListener';
-import useSmoothAnimation from './useSmoothAnimation';
-import { canUseDOM } from './utils';
+import { useEffect, useState } from 'react';
+import { canUseDOM, managedEventListener, smoothAnimation } from './utils';
 
 export default function useWindowSize() {
   const [size, setSize] = useState(
     canUseDOM ? [window.innerWidth, window.innerHeight] : [0, 0],
   );
 
-  useEventListener(
-    'resize',
-    useSmoothAnimation(() => {
-      setSize([window.innerWidth, window.innerHeight]);
-    }),
+  useEffect(
+    () =>
+      managedEventListener(
+        window,
+        'resize',
+        smoothAnimation(() => {
+          setSize([window.innerWidth, window.innerHeight]);
+        }),
+      ),
+    [],
   );
 
   return size;
