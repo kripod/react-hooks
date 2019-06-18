a408be3c9519d2f6bd61c423fdd18ca263c3f519
Kristóf Poduszló <kripod@protonmail.com>
2019-06-18 17:50:57 +0200

    Simplify useWindowScrollPosition test

--- src/useWindowScrollPosition.test.tsx
+++ src/useWindowScrollPosition.test.tsx
@@ -3,20 +3,15 @@ import { act, renderHook } from 'react-hooks-testing-library';
 import { useWindowScrollPosition } from '.';
 
 test('change window scroll position', () => {
-  const { result, waitForNextUpdate, unmount } = renderHook(() =>
-    useWindowScrollPosition(),
-  );
+  const { result, unmount } = renderHook(() => useWindowScrollPosition());
   expect(result.current.join()).toBe('0,0');
 
   act(() => {
     (window.scrollX as number) = 123;
     (window.scrollY as number) = 456;
     fireEvent.scroll(window);
-
-    (async () => {
-      await waitForNextUpdate();
-      expect(result.current.join()).toBe('123,456');
-      unmount();
-    })();
   });
+
+  expect(result.current.join()).toBe('123,456');
+  unmount();
 });
