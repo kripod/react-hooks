7fafbd68f08bc97d8f8cf0057f58d23d7afa7311
Kristóf Poduszló <kripod@protonmail.com>
2019-06-18 23:24:18 +0200

    Add TypeDoc comments for device hooks

--- packages/standard-hooks/src/useDeviceMotion.ts
+++ packages/standard-hooks/src/useDeviceMotion.ts
@@ -8,6 +8,15 @@ const initialState: Omit<DeviceMotionEvent, keyof Event> = {
   interval: 0,
 };
 
+/**
+ * Tracks acceleration and rotation rate of the device.
+ * @returns {DeviceMotionEvent} Defaults to an instance of [`DeviceMotionEventInit`](https://w3c.github.io/deviceorientation/#dictdef-devicemotioneventinit) constructed without parameters.
+ * @example
+ * const Example = () => {
+ *   const { acceleration, rotationRate, interval } = useDeviceMotion();
+ *   // ...
+ * };
+ */
 export default function useDeviceMotion() {
   const [motion, setMotion] = useState(initialState);
 
--- packages/standard-hooks/src/useDeviceOrientation.ts
+++ packages/standard-hooks/src/useDeviceOrientation.ts
@@ -8,6 +8,15 @@ const initialState: Omit<DeviceOrientationEvent, keyof Event> = {
   absolute: false,
 };
 
+/**
+ * Tracks physical orientation of the device.
+ * @returns {DeviceOrientationEvent} Defaults to an instance of [`DeviceOrientationEventInit`](https://w3c.github.io/deviceorientation/#dictdef-deviceorientationeventinit) constructed without parameters.
+ * @example
+ * const Example = () => {
+ *   const { alpha, beta, gamma, absolute } = useDeviceOrientation();
+ *   // ...
+ * };
+ */
 export default function useDeviceOrientation() {
   const [orientation, setOrientation] = useState(initialState);
 
