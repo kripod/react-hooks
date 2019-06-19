7220f3b3e5bb88861b48b4c57d816b9844185d47
Kristóf Poduszló <kripod@protonmail.com>
2019-06-19 20:33:05 +0200

    Add JSDoc comment for useEventListener

--- packages/standard-hooks/src/useEventListener.ts
+++ packages/standard-hooks/src/useEventListener.ts
@@ -1,6 +1,26 @@
 import { useEffect } from 'react';
 import { managedEventListener, useEventCallback } from './utils';
 
+/**
+ * Listens to an event while the enclosing component is mounted.
+ *
+ * ⚠️ Due to concerns about performance, using this hook is not advised unless event listener creation parameters may vary.
+ *
+ * @param type Name of event (case-sensitive).
+ * @param callback Method to execute whenever the event fires.
+ * @param target Target to listen on, possibly an element or a remote API service.
+ * @param options Additional listener characteristics.
+ *
+ * @example
+ * const Example = () => {
+ *   useEventListener('error', () => {
+ *     console.log('A resource failed to load.');
+ *   });
+ *   // ...
+ * };
+ *
+ * @see [Event reference on MDN](https://developer.mozilla.org/en-US/docs/Web/Events)
+ */
 export default function useEventListener(
   type: string,
   callback: EventListener,
