39dac20eb3cd7a3164cf4cb88db87c5c21d63a0e
Kristóf Poduszló <kripod@protonmail.com>
2019-08-18 23:19:36 +0200

    Add JSDoc annotation for usePreferredLanguages

--- src/usePreferredLanguages.ts
+++ src/usePreferredLanguages.ts
@@ -5,6 +5,18 @@ function getPreferredLanguages() {
   return navigator.languages || [navigator.language];
 }
 
+/**
+ * Tracks preferred languages of the user.
+ *
+ * @param defaultValue Fallback value used when preferences are unavailable.
+ * @returns {string[]} An array of [BCP 47](https://tools.ietf.org/html/bcp47) language tags, ordered by preference with the most preferred language first.
+ *
+ * @example
+ * const Example = () => {
+ *   const preferredLanguages = usePreferredLanguages();
+ *   // ...
+ * };
+ */
 export default function usePreferredLanguages(defaultValue = ['en-US']) {
   const [languages, setLanguages] = useState(
     canUseDOM ? getPreferredLanguages() : defaultValue,
