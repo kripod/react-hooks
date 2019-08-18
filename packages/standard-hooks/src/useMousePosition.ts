a839377db765ea8d2537a20fac20be4a288a9f59
Kristóf Poduszló <kripod@protonmail.com>
2019-08-18 11:37:29 +0200

    Add JSDoc annotations for tuple-returning hooks

--- packages/standard-hooks/src/useMousePosition.ts
+++ packages/standard-hooks/src/useMousePosition.ts
@@ -1,6 +1,17 @@
 import { useEffect, useState } from 'react';
 import { managedEventListener } from './utils';
 
+/**
+ * Tracks mouse position.
+ *
+ * @returns {[number, number]} Coordinates `[x, y]`, falling back to `[0, 0]` when unavailable.
+ *
+ * @example
+ * const Example = () => {
+ *   const [mouseX, mouseY] = useMousePosition();
+ *   // ...
+ * };
+ */
 export default function useMousePosition() {
   const [position, setPosition] = useState([0, 0]);
 
