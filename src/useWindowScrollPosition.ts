083eb8578206e40a7eab27576cac35ce3446f5ac
Kristóf Poduszló <kripod@protonmail.com>
2019-08-18 11:37:29 +0200

    Add JSDoc annotations for tuple-returning hooks

--- src/useWindowScrollPosition.ts
+++ src/useWindowScrollPosition.ts
@@ -1,6 +1,17 @@
 import { useEffect, useState } from 'react';
 import { canUseDOM, managedEventListener } from './utils';
 
+/**
+ * Tracks window scroll position.
+ *
+ * @returns {[number, number]} Coordinates `[x, y]`, falling back to `[0, 0]` when unavailable.
+ *
+ * @example
+ * const Example = () => {
+ *   const [windowScrollX, windowScrollY] = useWindowScrollPosition();
+ *   // ...
+ * };
+ */
 export default function useWindowScrollPosition() {
   const [position, setPosition] = useState(
     canUseDOM ? [window.scrollX, window.scrollY] : [0, 0],
--- src/useWindowSize.ts
+++ src/useWindowSize.ts
@@ -1,6 +1,17 @@
 import { useEffect, useState } from 'react';
 import { canUseDOM, managedEventListener } from './utils';
 
+/**
+ * Tracks window size.
+ *
+ * @returns {[number, number]} Dimensions `[width, height]`, falling back to `[0, 0]` when unavailable.
+ *
+ * @example
+ * const Example = () => {
+ *   const [windowWidth, windowHeight] = useWindowSize();
+ *   // ...
+ * };
+ */
 export default function useWindowSize() {
   const [size, setSize] = useState(
     canUseDOM ? [window.innerWidth, window.innerHeight] : [0, 0],
