2278c887e6154427e959040c28929dc2b0fd7f3d
Kristóf Poduszló <kripod@protonmail.com>
2019-08-19 11:08:02 +0200

    Remove useEventListener performance warning

--- packages/standard-hooks/src/useEventListener.ts
+++ packages/standard-hooks/src/useEventListener.ts
@@ -4,8 +4,6 @@ import { managedEventListener, useEventCallback } from './utils';
 /**
  * Listens to an event while the enclosing component is mounted.
  *
- * ⚠️ Due to concerns about performance, using this hook is not advised unless event listener creation parameters may vary.
- *
  * @param type Name of event (case-sensitive).
  * @param callback Method to execute whenever the event fires.
  * @param [target=window] Target to listen on, possibly a DOM element or a remote service connector.
