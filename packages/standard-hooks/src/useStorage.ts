598b49a6f213a61b83326c16931c9aa0d7b07db0
Kristóf Poduszló <kripod@protonmail.com>
2019-08-21 23:05:55 +0200

    Add inline docs for useStorage

--- packages/standard-hooks/src/useStorage.ts
+++ packages/standard-hooks/src/useStorage.ts
@@ -1,5 +1,16 @@
 import { useReducer } from 'react';
 
+/**
+ * Stores a key/value pair statefully.
+ *
+ * @param key Identifier to associate the stored value with.
+ * @param defaultValue Value used when no item exists with the given key.
+ * @param storage Storage object, which stays intact through page loads.
+ * @param errorCallback Method to execute in case of an error, e.g. when the storage quota has been exceeded.
+ * @returns A statefully stored value, and a function to update it. Update value to `null` for removal from the storage object.
+ *
+ * @see [`useState` hook](https://reactjs.org/docs/hooks-reference.html#usestate), which exposes a similar interface
+ */
 export default function useStorage(
   key: string,
   defaultValue: string | null = null,
