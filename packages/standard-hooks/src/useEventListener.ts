667e80031a5a951b7dd675fa0e51c311a761c0b3
Kristóf Poduszló <kripod@protonmail.com>
2019-08-23 16:21:51 +0200

    Require target explicitly in useEventListener

--- packages/standard-hooks/src/useEventListener.ts
+++ packages/standard-hooks/src/useEventListener.ts
@@ -4,9 +4,9 @@ import { EventMap, managedEventListener, useEventCallback } from './utils';
 /**
  * Listens to an event while the enclosing component is mounted.
  *
+ * @param target Target to listen on, possibly a DOM element or a remote service connector.
  * @param type Name of event (case-sensitive).
  * @param callback Method to execute whenever the event fires.
- * @param [target=window] Target to listen on, possibly a DOM element or a remote service connector.
  * @param options Additional listener characteristics.
  *
  * @example
@@ -23,16 +23,18 @@ export default function useEventListener<
   T extends EventTarget,
   K extends keyof EventMap<T> & string
 >(
+  target: T,
   type: K,
   callback: (event: EventMap<T>[K]) => void,
-  target?: T,
   options?: AddEventListenerOptions,
 ) {
   // Based on the implementation of `useInterval`
   const savedCallback = useEventCallback(callback);
 
-  useEffect(
-    () => managedEventListener(target || window, type, savedCallback, options),
-    [options, savedCallback, target, type],
-  );
+  useEffect(() => managedEventListener(target, type, savedCallback, options), [
+    options,
+    savedCallback,
+    target,
+    type,
+  ]);
 }
