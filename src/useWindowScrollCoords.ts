8b706f0bb84263152c55a35053783f50d3a9f7f8
Kristóf Poduszló <kripod@protonmail.com>
2019-08-24 07:11:32 +0200

    Rename position to coords for window scroll

--- src/useWindowScrollCoords.ts
+++ src/useWindowScrollCoords.ts
@@ -8,22 +8,22 @@ import { canUseDOM, managedEventListener } from './utils';
  *
  * @example
  * const Example = () => {
- *   const [windowScrollX, windowScrollY] = useWindowScrollPosition();
+ *   const [windowScrollX, windowScrollY] = useWindowScrollCoords();
  *   // ...
  * };
  */
-export default function useWindowScrollPosition(): Readonly<[number, number]> {
-  const [position, setPosition] = useState<Readonly<[number, number]>>(
+export default function useWindowScrollCoords(): Readonly<[number, number]> {
+  const [coords, setCoords] = useState<Readonly<[number, number]>>(
     canUseDOM ? [window.scrollX, window.scrollY] : [0, 0],
   );
 
   useEffect(
     () =>
       managedEventListener(window, 'scroll', () => {
-        setPosition([window.scrollX, window.scrollY]);
+        setCoords([window.scrollX, window.scrollY]);
       }),
     [],
   );
 
-  return position;
+  return coords;
 }
