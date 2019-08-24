dd7944f8f9716b486c08760c7a2761413773ca47
Kristóf Poduszló <kripod@protonmail.com>
2019-08-24 23:51:46 +0200

    Add test for useNetworkAvailability

---
+++ packages/standard-hooks/src/useNetworkAvailability.test.ts
@@ -0,0 +1,13 @@
+import { fireEvent } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useNetworkAvailability } from '.';
+
+test('change network availability', () => {
+  const { result } = renderHook(() => useNetworkAvailability());
+  expect(result.current).toBe(true);
+
+  act(() => {
+    fireEvent(window, new Event('offline'));
+  });
+  expect(result.current).toBe(false);
+});
