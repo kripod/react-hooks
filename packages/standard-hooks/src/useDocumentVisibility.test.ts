5d423f3c1739f825b7a9120f0e02280785cc1470
Kristóf Poduszló <kripod@protonmail.com>
2019-08-24 23:48:19 +0200

    Add test for useDocumentVisibility

---
+++ packages/standard-hooks/src/useDocumentVisibility.test.ts
@@ -0,0 +1,14 @@
+import { fireEvent } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useDocumentVisibility } from '.';
+
+test('change document visibility', () => {
+  const { result } = renderHook(() => useDocumentVisibility());
+  expect(result.current).toBe('visible');
+
+  act(() => {
+    Object.defineProperty(document, 'visibilityState', { value: 'hidden' });
+    fireEvent(document, new Event('visibilitychange'));
+  });
+  expect(result.current).toBe('hidden');
+});
