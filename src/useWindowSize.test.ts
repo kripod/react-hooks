555d1f53671eb27aab910d232008581066c1f339
Kristóf Poduszló <kripod@protonmail.com>
2019-08-24 06:55:30 +0200

    Add test for useWindowSize

---
+++ src/useWindowSize.test.ts
@@ -0,0 +1,15 @@
+import { fireEvent } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useWindowSize } from '.';
+
+test('change window size', () => {
+  const { result } = renderHook(() => useWindowSize());
+  expect(result.current).toEqual([1024, 768]);
+
+  act(() => {
+    (window.innerWidth as number) = 1920;
+    (window.innerHeight as number) = 1080;
+    fireEvent(window, new UIEvent('resize'));
+  });
+  expect(result.current).toEqual([1920, 1080]);
+});
