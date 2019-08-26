ee73a43365fcd615622abbe38cfebcfb3941da3d
Kristóf Poduszló <kripod@protonmail.com>
2019-08-26 20:33:37 +0200

    Provide dummy parameters for SSR tests

--- packages/standard-hooks/src/ssr.test.tsx
+++ packages/standard-hooks/src/ssr.test.tsx
@@ -16,8 +16,16 @@ function renderHookToString<T>(callback: () => T) {
   return renderToString(<Hook callback={callback} />);
 }
 
-test.each(Object.entries(hooks))('%s supports SSR', (_name, callback) => {
-  // TODO: Provide dummy parameters for hooks which need them
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  renderHookToString(callback as any);
+test.each(
+  Object.entries({
+    // Provide dummy parameters for hooks which need them
+    ...hooks,
+    useEventListener: () =>
+      hooks.useEventListener(new EventTarget(), 'foo', () => {}),
+    useGeolocation: () => hooks.useGeolocation(),
+    useInterval: () => hooks.useInterval(() => {}, 0),
+    useStorage: () => hooks.useStorage('foo'),
+  }),
+)('%s supports SSR', (_name, callback) => {
+  renderHookToString(callback);
 });
