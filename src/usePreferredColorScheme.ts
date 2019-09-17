8281520ec38534983a1ca0ac9206ec5f0cd2d813
cmoog <moogcharlie@gmail.com>
2019-09-16 21:31:35 -0500

    adds usePreferredColorScheme hook

---
+++ src/usePreferredColorScheme.ts
@@ -0,0 +1,28 @@
+import { useMedia } from '.';
+
+/**
+ * Tracks color scheme preference.
+ *
+ * @returns {'light' | 'dark' | 'no-preference' | null} isMatch
+ *
+ * @example
+ * function Example() {
+ *   const const preferredColorScheme = usePreferredColorScheme();
+ *   const isDarkMode = usePreferredColorScheme() === 'dark';
+ *   // ...
+ * }
+ */
+export default function usePreferredColorScheme():
+  | 'dark'
+  | 'light'
+  | 'no-preference'
+  | null {
+  const isLight = useMedia('(prefers-color-scheme: light)');
+  const isDark = useMedia('(prefers-color-scheme: dark)');
+  const noPreference = useMedia('(prefers-color-scheme: no-preference)');
+
+  if (isLight) return 'light';
+  if (isDark) return 'dark';
+  if (noPreference) return 'no-preference';
+  return null;
+}
