5ab51b5375733267c5e92494becac46deff1de7e
Kristóf Poduszló <kripod@protonmail.com>
2019-09-20 14:45:06 +0200

    Add examples for 'usePrevious' and 'useTimeline'

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -5,6 +5,14 @@ import { useEffect, useRef } from 'react';
  *
  * @param value Props, state or any other calculated value.
  * @returns Value from the previous render of the enclosing component.
+ *
+ * @example
+ * function Example() {
+ *   const [count, setCount] = useState(0);
+ *   const prevCount = usePrevious(count);
+ *   // ...
+ *   return `Now: ${count}, before: ${prevCount}`;
+ * }
  */
 export default function usePrevious<T>(value: T): T {
   // Source: https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
--- src/useTimeline.ts
+++ src/useTimeline.ts
@@ -8,6 +8,14 @@ import { MAX_ARRAY_INDEX } from './utils';
  * @param value Props, state or any other calculated value.
  * @param maxLength Maximum amount of states to store. Should be an integer more than 1.
  * @returns Results of state updates in chronological order.
+ *
+ * @example
+ * function Example() {
+ *   const [count, setCount] = useState(0);
+ *   const counts = useTimeline(count);
+ *   // ...
+ *   return `Now: ${count}, history: ${counts}`;
+ * }
  */
 export default function useTimeline<T>(
   value: T,
