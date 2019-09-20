39480a72ee728acf5687b192acc11a8ff9af218e
Kristóf Poduszló <kripod@protonmail.com>
2019-09-20 13:56:11 +0200

    Add missing source comment to 'usePrevious'

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -7,6 +7,7 @@ import { useEffect, useRef } from 'react';
  * @returns Value from the previous render of the enclosing component.
  */
 export default function usePrevious<T>(value: T): T {
+  // Source: https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
   const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
