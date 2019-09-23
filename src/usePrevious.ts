e97ec72f1eefbc600a9f077f799ecbc7cc295af9
Kristóf Poduszló <kripod@protonmail.com>
2019-09-23 10:42:48 +0200

    Fix type declaration of 'usePrevious'

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -14,11 +14,11 @@ import { useEffect, useRef } from 'react';
  *   return `Now: ${count}, before: ${prevCount}`;
  * }
  */
-export default function usePrevious<T>(value: T): T {
+export default function usePrevious<T>(value: T): T | undefined {
   // Source: https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
   const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
   });
-  return ref.current as T;
+  return ref.current;
 }
