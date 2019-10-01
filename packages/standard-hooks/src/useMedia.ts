126a3dcb5aac9cd9f92f863259879b63a49f6cf9
Kristóf Poduszló <kripod@protonmail.com>
2019-10-01 21:36:13 +0200

    Remove redundant JSDoc return type

--- packages/standard-hooks/src/useMedia.ts
+++ packages/standard-hooks/src/useMedia.ts
@@ -6,7 +6,7 @@ import { canUseDOM, managedEventListener } from './utils';
  *
  * @param query Media query to parse.
  *
- * @returns {boolean} `true` if the associated media query list matches the state of the [`document`](https://developer.mozilla.org/docs/Web/API/Document), or `false` otherwise.
+ * @returns `true` if the associated media query list matches the state of the [`document`](https://developer.mozilla.org/docs/Web/API/Document), or `false` otherwise.
  *
  * @example
  * function Example() {
