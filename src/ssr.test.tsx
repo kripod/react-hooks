123da5a432913bcb77d3e0ab82f86d0594181e03
Kristóf Poduszló <kripod@protonmail.com>
2019-10-10 12:00:08 +0200

    Remove redundant type assertion

--- src/ssr.test.tsx
+++ src/ssr.test.tsx
@@ -34,7 +34,7 @@ test.each(
     useSessionStorage: () => hooks.useSessionStorage('foo'),
     useTimeline: () => hooks.useTimeline(0),
     useUndoable: () => hooks.useUndoable(useState()),
-    useToggle: () => hooks.useToggle(useState(false as boolean)),
+    useToggle: () => hooks.useToggle(useState(false)),
   }),
 )('%s supports SSR', (_name, callback) => {
   renderHookToString(callback);
